71dc8e63526e6412e0a4808bf5ed4e4f703498b2
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-09-11 22:24:38 -0500

    Updates

---
+++ precache-manifest.c2f4b3ed4384b8c49d38e2752066e5f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45d3bab02030ca24152f78f8b94542c",
+    "url": "/Beauty-Quotient.io/deploy-react/index.html"
+  },
+  {
+    "revision": "f0c9cc4f873bf0fbe1ea",
+    "url": "/Beauty-Quotient.io/deploy-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "80fe20a84537f9ffc1a5",
+    "url": "/Beauty-Quotient.io/deploy-react/static/js/2.1c800261.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Beauty-Quotient.io/deploy-react/static/js/2.1c800261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c9cc4f873bf0fbe1ea",
+    "url": "/Beauty-Quotient.io/deploy-react/static/js/main.22b33049.chunk.js"
+  },
+  {
+    "revision": "1d4ac00cac697975bfed",
+    "url": "/Beauty-Quotient.io/deploy-react/static/js/runtime-main.eaba7eb3.js"
+  }
+]);
